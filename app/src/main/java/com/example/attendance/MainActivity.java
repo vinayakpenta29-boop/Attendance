77cccc93c7b7e24ce2c75386b8d2dedbcdd3f24f
vinayakpenta29-boop <vinayakpenta29@gmail.com>
2026-03-06 09:32:33 +0530

    Create MainActivity.java

---
+++ app/src/main/java/com/example/attendance/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.attendance;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = findViewById(R.id.tabs);
+        viewPager = findViewById(R.id.viewPager);
+
+        adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new DailyAttendanceFragment(), "Daily Attendance");
+        adapter.addFragment(new ViewAttendanceFragment(), "View Attendance");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
